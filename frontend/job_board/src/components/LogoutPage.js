c92da18fa9e61c4eee0c0f35d5e063e0a6233c1b
Dimitris <jim600v@gmail.com>
2023-12-16 04:06:37 +0200

    feat: jwt authentication

---
+++ frontend/job_board/src/components/LogoutPage.js
@@ -0,0 +1,18 @@
+import { useEffect,useState } from "react";
+import axios from 'axios';
+
+
+const Logout = () => {
+    useEffect(() => {
+        (async ()=>{
+
+        })
+    },[]);
+    return(
+        <div>
+            <h6>See you around!</h6>
+        </div>
+    );
+};
+
+export default Logout;
---
+++ frontend/job_board/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom";
+
+
+const NavBar = () => {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
+            <Link class="navbar-brand" to="/HomePage">Home</Link>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><Link class="nav-link" to="/singup">Sign Up</Link></li>
+                <li class="nav-item"><Link class="nav-link" to="/login">Login</Link></li>
+                <li class="nav-item"><Link class="nav-link" to="/profile">Profile</Link></li>
+            </ul>
+        </nav>
+    );
+};
+
+
+export default NavBar;
